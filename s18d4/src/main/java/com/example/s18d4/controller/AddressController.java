d107cdf25b5e1e8d227e8250ae80ff74491d616f
skaimarvas <skaimarvas@gmail.com>
2024-02-16 11:01:33 +0300

    methods were added

--- s18d4/src/main/java/com/example/s18d4/controller/AddressController.java
+++ s18d4/src/main/java/com/example/s18d4/controller/AddressController.java
@@ -1,4 +1,36 @@
 package com.example.s18d4.controller;
 
+
+import com.example.s18d4.entity.Address;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("workintech/address")
 public class AddressController {
+
+
+    @GetMapping
+    public List<Address> findAll(){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Address findAdress(@PathVariable int id){
+        return null;
+    }
+
+    @PostMapping
+    public void addAddress(@RequestBody Address address){
+
+    }
+    @PutMapping("/{id}")
+    public void updateAddress(){
+
+    }
+    @DeleteMapping("/{id}")
+    public void deleteAddress(){
+
+    }
 }
