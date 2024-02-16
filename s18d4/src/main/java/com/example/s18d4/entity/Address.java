683e89507999d9513178f54775702f74a874e187
skaimarvas <skaimarvas@gmail.com>
2024-02-16 09:23:08 +0300

    fields and annotations were  added

--- s18d4/src/main/java/com/example/s18d4/entity/Address.java
+++ s18d4/src/main/java/com/example/s18d4/entity/Address.java
@@ -1,4 +1,29 @@
 package com.example.s18d4.entity;
 
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "adress", schema = "fsweb")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "no")
+    private int no;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "country")
+    private String country;
+    @Nullable
+    @Column(name = "description")
+    private String description;
+
 }
