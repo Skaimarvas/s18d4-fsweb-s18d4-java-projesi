2316bca9b0ec56caac52dc40b0e09a9cb721da59
skaimarvas <skaimarvas@gmail.com>
2024-02-16 09:48:21 +0300

    fields and annotations were  added

--- s18d4/src/main/java/com/example/s18d4/entity/Customer.java
+++ s18d4/src/main/java/com/example/s18d4/entity/Customer.java
@@ -1,4 +1,25 @@
 package com.example.s18d4.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="customer", schema="fsweb")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "salary")
+    private double salary;
+    @Column(name = "address_id")
+    private int addressId;
 }
