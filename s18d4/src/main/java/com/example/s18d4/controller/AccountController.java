4b6397c98b7640ac4dec2cac42bb493ebaf40f44
skaimarvas <skaimarvas@gmail.com>
2024-02-17 18:46:47 +0300

    accountService and customerService were added as fields. AccountController constructor was created. All methods were updated

--- s18d4/src/main/java/com/example/s18d4/controller/AccountController.java
+++ s18d4/src/main/java/com/example/s18d4/controller/AccountController.java
@@ -1,7 +1,11 @@
 package com.example.s18d4.controller;
 
+import com.example.s18d4.dto.AccountResponse;
+import com.example.s18d4.dto.CustomerResponse;
 import com.example.s18d4.entity.Account;
 import com.example.s18d4.entity.Customer;
+import com.example.s18d4.service.AccountService;
+import com.example.s18d4.service.CustomerService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,26 +13,61 @@ import java.util.List;
 @RestController
 @RequestMapping("workintech/account")
 public class AccountController {
+    private final AccountService accountService;
+    private final CustomerService customerService;
+    public AccountController(AccountService accountService, CustomerService customerService){
+        this.accountService = accountService;
+        this.customerService = customerService;
+    }
     @GetMapping
     public List<Account> findAll(){
-        return null;
+        return accountService.findAll();
     }
-
     @GetMapping("/{id}")
     public Account findAccount(@PathVariable int id){
-        return null;
-    }
-
-    @PostMapping
-    public void addAccount(@RequestBody Account account){
+        return accountService.find(id);
 
     }
-    @PutMapping("/{id}")
-    public void updateAccount(){
-
+    @PostMapping("/{customerId}")
+    public AccountResponse save(@RequestBody Account account, @PathVariable long customerId){
+        Customer customer = customerService.find(customerId);
+        if(customer != null){
+            customer.getAccounts().add(account);
+            account.setCustomer(customer);
+            accountService.save(account);
+        } else {
+            throw new RuntimeException("No customer found!");
+        }
+        return new AccountResponse(account.getId(),account.getAccountName(), account.getMoneyAmount(), new CustomerResponse(customer.getId(),customer.getEmail(),customer.getSalary()));
     }
+    @PutMapping("/{customerId}")
+    public AccountResponse update(@RequestBody Account account, @PathVariable long customerId){
+      Customer customer = customerService.find(customerId);
+      Account foundAccount = null;
+      for (Account account1: customer.getAccounts()){
+          if (account.getId() == account1.getId()){
+              foundAccount = account1;
+          }
+      }
+      if (foundAccount == null ){
+          throw new RuntimeException("Account(" + account.getId() + ") not found for this customer: " + customerId);
+      }
+      int indexOfFound = customer.getAccounts().indexOf(foundAccount);
+      customer.getAccounts().set(indexOfFound,account);
+      account.setCustomer(customer);
+      accountService.save(account);
+      return new AccountResponse(account.getId(),account.getAccountName(), account.getMoneyAmount(), new CustomerResponse(customer.getId(), customer.getEmail(), customer.getSalary()));
+    }
+
     @DeleteMapping("/{id}")
-    public void deleteAccount(){
+    public AccountResponse remove(@PathVariable long id){
+        Account account = accountService.find(id);
+        if(account != null ){
+            accountService.delete(id);
+            return new AccountResponse(account.getId(), account.getAccountName(), account.getMoneyAmount(), new CustomerResponse(account.getCustomer().getId(),account.getCustomer().getEmail(), account.getCustomer().getSalary()));
+        } else  {
+            throw new RuntimeException("No account found");
+        }
 
     }
 }
