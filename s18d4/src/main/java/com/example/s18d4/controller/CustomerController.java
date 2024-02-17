ba297882be1231ff0c9c67b8b0ab27fd78e664e7
skaimarvas <skaimarvas@gmail.com>
2024-02-17 18:48:36 +0300

    CustomerController was added and all methods were updated

--- s18d4/src/main/java/com/example/s18d4/controller/CustomerController.java
+++ s18d4/src/main/java/com/example/s18d4/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.example.s18d4.controller;
 
 
+import com.example.s18d4.dto.CustomerResponse;
 import com.example.s18d4.entity.Customer;
+import com.example.s18d4.service.CustomerService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,26 +11,50 @@ import java.util.List;
 @RestController
 @RequestMapping("workintech/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+
+    }
     @GetMapping
     public List<Customer> findAll(){
-        return null;
+        return customerService.findAll();
     }
 
     @GetMapping("/{id}")
     public Customer findCustomer(@PathVariable int id){
-        return null;
+        return customerService.find(id);
     }
 
     @PostMapping
-    public void addCustomer(@RequestBody Customer customer){
-
+    public CustomerResponse save(@RequestBody Customer customer){
+        if (customer != null){
+            customerService.save(customer);
+        } else {
+            throw new RuntimeException("Customer object is empty!");
+        }
+        return new CustomerResponse(customer.getId(), customer.getEmail(), customer.getSalary());
     }
     @PutMapping("/{id}")
-    public void updateCustomer(){
+    public CustomerResponse update(@RequestBody Customer customer, @PathVariable long id){
+          Customer customerFound = customerService.find(id);
+          if(customerFound == null){
+              throw new RuntimeException("Customemr(" + id + ") not found");
+          }
+          customerFound = customer;
 
+          return new CustomerResponse(customerFound.getId(), customerFound.getEmail(), customerFound.getSalary());
     }
     @DeleteMapping("/{id}")
-    public void deleteCustomer(){
-
+    public CustomerResponse remove(@PathVariable long id){
+         Customer customer = customerService.find(id);
+         if (customer != null){
+             customerService.delete(id);
+             return new CustomerResponse(customer.getId(), customer.getEmail(), customer.getSalary());
+         } else {
+             throw new RuntimeException("No customer found");
+         }
     }
 }
